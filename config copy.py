d71a7173e902755915e8fbea050c313f9c047b80
venugopal952 <kurva.venugopal2020@gmail.com>
2025-08-24 18:09:39 +0000

    update sample data

--- config copy.py
+++
@@ -1,8 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'anomaly_detection.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAX_CONTENT_LENGTH = 16 * 1024 * 1024  # Max upload size 16MB
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
--- routes copy/__init__.py
+++
@@ -1,5 +0,0 @@
-from flask import Blueprint
-
-anomaly_bp = Blueprint('anomaly', __name__)
-
-from routes import anomaly_routes
